789b9b317a3164b25749338d722bcb7a5e19cbf9
irenerecu <irenerecu@gmail.com>
2026-09-11 13:55:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,25 @@
 const CACHE_NAME = 'fichaje-vf-v1';
-const urlsToCache = [
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './img/vicky-foods.png',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+  './sw.js',
+  './icon-192.png',
+  './img/vicky-foods.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
